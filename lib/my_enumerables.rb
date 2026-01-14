6d7d2b35fb248242667925fef6f608b49b5d3479
Junior1511 <yunior122000@gmail.com>
2026-01-13 21:32:14 -0600

    This is very interesting, my_each was hard to think about and get done, I feel like something like this is pretty hard to think about, and get done unless test are in place.

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -8,4 +8,15 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    #returns enum if no block given
+    return to_enum(:my_each) unless block_given?
+    #should execute whatever block given 
+    #and the parameter is i and user should be able to use it 
+    for i in self
+      yield(i)
+    end
+    # should also return self at the end 
+    self
+  end
 end
