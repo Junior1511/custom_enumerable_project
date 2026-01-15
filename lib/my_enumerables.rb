e8b369477ebfad6e3530fda824d00ae14802aee0
Junior1511 <yunior122000@gmail.com>
2026-01-15 05:50:59 -0600

    this is really hard, but interestin, I wish I had more time

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,35 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    #returns enum if no block given
+    return to_enum(:my_each_with_index) unless block_given?
+    
+    index = 0
+    for i in self
+      yield(i, index)
+      index +=1 unless index > self.length
+    end
+    # should also return self at the end 
+    self
+  end
+
+  def my_select
+    #returns enum if no block given
+    return to_enum(:my_select) unless block_given?
+    
+    array = []
+    for i in self
+      if yield(i) == true
+        array << i
+      end
+    end
+    #returns new array with truthy values
+    array
+  end
+  
+  def my_all?
+    true unless self.include?(false, nil) 
+  end
 end
 
 # You will first have to define my_each
